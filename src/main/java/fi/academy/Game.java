c1852841b40aa6d2f9c14e44fb20830bb20aff0f
JunttilaJoonas <junttila.joonas@gmail.com>
2018-06-26 12:21:53 +0300

    joonas

--- src/main/java/fi/academy/Game.java
+++ src/main/java/fi/academy/Game.java
@@ -12,39 +12,62 @@ public class Game {
 
     public static void main( String[] args ) {
         Game game = new Game();
-        game.elevator();
+        game.elevator1();
     }
 
-    public void elevator(){
+    public void elevator1(){ //JOONAS
         if (!avain) {
             System.out.println("You are in an elevator. What do you want to do?");
             System.out.println("1: Go right");
             System.out.println("2: Go left");
-            int choice = sc.nextInt();
+            String choice = sc.nextLine();
 
-            if (choice==1) {
+            if (choice.equalsIgnoreCase("1")) {
                 hallway6();
             }
 
-            if (choice==2) {
+            if (choice.equalsIgnoreCase("2")) {
                 office();
             }
+
+            else {
+                System.out.println("lol");
+                elevator1();
+            }
         }
-
-
-
-
     }
 
-    private void office() {
+    public void conference17(){ //JOONAS
+        System.out.println("You are in a room. There is currently nothing interesting in this room.");
+        System.out.println("1: Go back to the hallway.");
+        String choice = sc.nextLine();
+        if (choice.equalsIgnoreCase("1")) {
+            hallway18();
+        }
+    }
+
+    public void hallway18(){ //JOONAS
+        System.out.println("You are in a hallway of some sort.");
+    }
+
+    public void conference19(){ //JOONAS
+        System.out.println("You are in a room. There is currently nothing interesting in this room.");
+        System.out.println("1: Go back to the hallway.");
+        String choice = sc.nextLine();
+        if (choice.equalsIgnoreCase("1")) {
+            hallway18();
+        }
+    }
+
+    public void office() { //JOONAS
         System.out.println("You arrived in the office. What would you like to do now?");
     }
 
-    private void hallway6() {
+    public void hallway6() { //JOONAS
         System.out.println("Joonaksen editti");
     }
 
-    private void toilet24() {
+    public void toilet24() {
         System.out.println("You can't enter the loo, you fool!");
     }
 }
