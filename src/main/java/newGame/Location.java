45228fc23cc4354e37fe212f94a0aedbbf018977
JunttilaJoonas <junttila.joonas@gmail.com>
2018-06-27 13:06:39 +0300

    location-luokkaa päivitelty

--- src/main/java/newGame/Location.java
+++ src/main/java/newGame/Location.java
@@ -1,4 +1,51 @@
 package newGame;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Location {
+    private String name;
+    private String description;
+    private Map<Integer, String> exits = new HashMap<>();
+    private List<Item> items = new ArrayList<>();
+    private boolean locked = false;
+    private boolean lockedWithPasscode = false;
+
+    public Location(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Location(String name, String description, boolean locked, boolean lockedWithPasscode) {
+        this.name = name;
+        this.description = description;
+        this.locked = locked;
+        this.lockedWithPasscode = lockedWithPasscode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<Integer, String> getExits() {
+        return exits;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public boolean isLockedWithPasscode() {
+        return lockedWithPasscode;
+    }
 }
