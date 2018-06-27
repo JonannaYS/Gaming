ced3e019e49921db572fbe2f908fff24a91f28c4
JunttilaJoonas <junttila.joonas@gmail.com>
2018-06-27 17:05:26 +0300

    viesti

--- src/main/java/newGame/Item.java
+++ src/main/java/newGame/Item.java
@@ -3,7 +3,7 @@ package newGame;
 public class Item {
     private String name;
     private String description;
-    private int weight;
+    private int weight = 0;
     private boolean movable;
 
     public Item(String name, String description, int weight) {
