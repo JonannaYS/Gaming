99dc3f5f2d6ab9fb3404d1e23e7f45b95bbd31cd
jounikoo <jouni.kullervo.kari@gmail.com>
2018-06-27 13:22:11 +0300

    class bag.java added

--- src/main/java/newGame/Item.java
+++ src/main/java/newGame/Item.java
@@ -1,6 +1,43 @@
 package newGame;
 
 public class Item {
-    private String Name;
+    private String name;
     private String description;
+    private int weight;
+    private boolean movable;
+
+    public Item(String name, String description, int weight) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+    }
+
+    public Item(String name, String description, int weight, boolean movable) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+        this.movable = movable;
+    }
+
+    //Minimum constructor for an item: Name and weight
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
